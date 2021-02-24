165f97a9c57b54cda28eecb86ea5f28729a47181
Michael Benford <michael@michaelbenford.net>
2021-02-24 00:40:14 -0300

    wip: add first draft

--- libqtile/layout/columns.py
+++ libqtile/layout/columns.py
@@ -479,3 +479,41 @@ class Columns(Layout):
                 col.heights[client] = 100
             col.width = 100
         self.group.layout_all()
+
+
+class ThreeColumns(layout.Columns):
+    defaults = [
+        ("main_ratio", .5,
+            "The percent of the screen-space the main window should occupy "
+            "by default")
+    ]
+
+    def __init__(self, **config):
+        layout.Columns.__init__(self, **config)
+        self.add_defaults(ThreeColumns.defaults)
+        self.num_columns = 3
+
+    def add(self, client):
+        super().add(client)
+
+        if len(self.columns) == 3:
+            main_width = 300 * self.main_ratio
+            other_width = (300 - main_width) / 2
+            self.columns[0].width = other_width
+            self.columns[1].width = main_width
+            self.columns[2].width = other_width
+            self.group.layout_all()
+
+    def remove(self, client):
+        super().remove(client)
+
+        if 0 < len(self.columns) < 3:
+            for col in self.columns:
+                col.width = 100
+            self.group.layout_all()
+
+    def add_column(self):
+        if len(self.columns) < 2:
+            return super().add_column(True)
+        elif len(self.columns) == 2:
+            return super().add_column(False)
